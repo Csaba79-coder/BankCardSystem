64c31e3b7b16813049f1f443eb4b4ee803e0fb32
Csaba79-coder <csabavadasz79@gmail.com>
2022-05-30 12:22:52 +0200

    Initial commit

---
+++ src/main/java/com/csaba79coder/bankcardsystem/BankCardSystemApplication.java
@@ -0,0 +1,13 @@
+package com.csaba79coder.bankcardsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankCardSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankCardSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/csaba79coder/bankcardsystem/BankCardSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csaba79coder.bankcardsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankCardSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
