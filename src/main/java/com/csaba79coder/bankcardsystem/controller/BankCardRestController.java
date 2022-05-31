de965e237f009b51d50841b9369a32a91968c5a4
Csaba79-coder <csabavadasz79@gmail.com>
2022-05-31 22:29:44 +0200

    todo: PatchMapping!

--- src/main/java/com/csaba79coder/bankcardsystem/controller/BankCardRestController.java
+++ src/main/java/com/csaba79coder/bankcardsystem/controller/BankCardRestController.java
@@ -33,4 +33,10 @@ public class BankCardRestController {
     public BankCard saveBankCardRestAPI() {
         return new BankCard();
     }
+
+    // TOD
+    @PatchMapping()
+    public void patch() {
+
+    }
 }
