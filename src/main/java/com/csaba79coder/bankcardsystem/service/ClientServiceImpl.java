c4265cead5fdbbebfbc52f0ec85776714042c31f
Csaba79-coder <csabavadasz79@gmail.com>
2022-06-01 16:26:43 +0200

    restore last working code, and saves from form to repository!

--- src/main/java/com/csaba79coder/bankcardsystem/service/ClientServiceImpl.java
+++ src/main/java/com/csaba79coder/bankcardsystem/service/ClientServiceImpl.java
@@ -57,13 +57,13 @@ public class ClientServiceImpl implements ClientService {
 
     @Override
     public String saveClientForm(Client client) {
-        String result = "";
         List<Client> clients = clientRepository.findAll();
         for (Client value : clients) {
             if (value.getEmail().equals(client.getEmail())) {
                 return "error";
             }
         }
+        clientRepository.save(client);
         return "success";
     }
 
