8cda4466e443981b4d09f541e521c4847302f353
Csaba79-coder <csabavadasz79@gmail.com>
2022-06-01 13:56:16 +0200

    button drives us to a new link that holds bankcard list (using bootstrap for view!)

--- src/main/java/com/csaba79coder/bankcardsystem/controller/BootstrapViewController.java
+++ src/main/java/com/csaba79coder/bankcardsystem/controller/BootstrapViewController.java
@@ -1,5 +1,6 @@
 package com.csaba79coder.bankcardsystem.controller;
 
+import com.csaba79coder.bankcardsystem.service.BankCardService;
 import com.csaba79coder.bankcardsystem.service.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,15 +11,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class BootstrapViewController {
 
     private final ClientService clientService;
+    private final BankCardService bankCardService;
 
     @Autowired
-    public BootstrapViewController(ClientService clientService) {
+    public BootstrapViewController(ClientService clientService, BankCardService bankCardService) {
         this.clientService = clientService;
+        this.bankCardService = bankCardService;
     }
 
     @GetMapping("/bootstrap/list")
     public String displayClientsHtml(Model model) {
         model.addAttribute("list", clientService.findAllClients());
         return "bootstrapview";
+    }
+
+    @GetMapping("/bootstrap/card/list")
+    public String displayCardsHtml(Model model) {
+        model.addAttribute("list", bankCardService.findAllBankCards());
+        return "cardbootstrapview";
     }
 }
